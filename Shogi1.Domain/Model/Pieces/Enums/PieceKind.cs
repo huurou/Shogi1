7f3e5a1cc910c860396616bcfc96a38ceabd4b74
kitada <kitada.taihei@Horizon.co.jp>
2021-09-19 00:05:14 +0900

    プロジェクト ファイルを追加します。

---
+++ Shogi1.Domain/Model/Pieces/Enums/PieceKind.cs
@@ -0,0 +1,21 @@
+﻿namespace Shogi1.Domain.Model.Pieces.Enums
+{
+    public enum PieceKind
+    {
+        Osho, // 王将
+        Gyokusho, // 玉将
+        Hisha, // 飛車
+        Ryuo, // 龍王
+        Kakugyo, // 角行
+        Ryuma, // 龍馬
+        Kinsho, // 金将
+        Ginsho, // 銀将
+        Narigin, // 成銀
+        Keima, // 桂馬
+        Narikei, // 成桂
+        Kyosha, // 香車
+        Narikyo, // 成香
+        Fuhyo, // 歩兵
+        Tokin, // と金
+    }
+}
