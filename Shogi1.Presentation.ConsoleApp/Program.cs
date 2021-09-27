2f8e1bc69938a09edb0070b6cb070365a8e73649
kitada <kitada.taihei@Horizon.co.jp>
2021-09-27 18:47:45 +0900

    プロジェクト追加

---
+++ Shogi1.Presentation.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shogi1.Presentation.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
